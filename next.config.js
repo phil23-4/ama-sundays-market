faf10f3178786049044c413e8d1088f290c4ea47
phil2021 <86551492+phil2021@users.noreply.github.com>
2023-02-06 02:00:16 +0800

    added remote patterns

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: { locales: ['en'], defaultLocale: 'en' },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**tailwindui.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**image.flaticon.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
