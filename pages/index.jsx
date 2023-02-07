3d7b8ce5fd7bb5936981c9f42ddab8238cbe8864
phil2021 <86551492+phil2021@users.noreply.github.com>
2023-02-07 18:00:53 +0800

    minor changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Registration from "../components/Forms/registrationForm";
 import Layout from "../components/layout";
 import Pricing from "../components/Pricing/pricing";
+import Hero from "../components/ui/hero";
 
 export default function Home() {
   return (
     <Layout>
+      <Hero />
       <Pricing />
       <Registration />
     </Layout>
