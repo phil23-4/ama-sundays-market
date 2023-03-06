f833a85b26fdbaad3821116704504ea79ba344a2
phil23-4 <philoc95@live.com>
2023-03-07 00:49:50 +0800

    updated meta tag

--- components/layout.jsx
+++ components/layout.jsx
@@ -11,6 +11,7 @@ export default function Layout({ children }) {
         <meta
           name="description"
           content="Decentralizing Farmers Markets for sustainable agricultural development that leaves no one and no place behind"
+          key="desc"
         />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
